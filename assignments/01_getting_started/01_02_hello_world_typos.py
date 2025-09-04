143c79a23c665d74482dc25ca47f5ff24a51508a
jamari_skinner <jskinnrr007@gmail.com>
2025-09-04 12:03:34 -0400

    Delete assignments/01_getting_started directory

--- assignments/01_getting_started/01_02_hello_world_typos.py
+++
@@ -1,9 +0,0 @@
-hello_world.py # type: ignore
-
-print ("Hello World! Get ready for PYTHON POWER!")
-
-message = "Hello Python World"
-print(message)
-
-message = "Hello Python Crash Course world!"
-print(message)
