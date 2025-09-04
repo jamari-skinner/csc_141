c7fa96618a5c31aa299db3ae597ca8b110b90aba
jamari_skinner <jskinnrr007@gmail.com>
2025-09-04 11:42:58 -0400

    Delete assignments/01_getting_started/csc_141 directory

--- assignments/01_getting_started/csc_141/python_work/01_03_infinite_skills.py
+++
@@ -1,9 +0,0 @@
-"""
-
-If I had infinitte programming skills, I'd try to be humble and just mod games and such.
-To get risky though, I'd make a password decrypter or code decrypter.
-Everything important to anyone has some type of code or guard, so everything would become open.
-Lastly, Just erase my identity from the world, or make a fake one as a cover up just on case.
-Just no consistencies.
-
-"""
