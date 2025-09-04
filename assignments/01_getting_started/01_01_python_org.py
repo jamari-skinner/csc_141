3e9a8e6babc3c4e89be416eecd836b360050b88c
jamari_skinner <jskinnrr007@gmail.com>
2025-09-04 11:45:20 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,9 @@
+# Some cool features of python.org are that it lets you easily download the latest version of Python.
+
+# Explore detailed beginner-friendly tutorials.
+
+# It has access full documentation. 
+
+# The site also has a “What’s New” section to keep developers updated on changes.
+
+# Plus community resources and guides for people who want to contribute to Python.
